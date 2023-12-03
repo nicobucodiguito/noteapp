266a3e39a776972aae6dff8892aaafe65a2024ae
nicobucodiguito <nicocobucoco@gmail.com>
2023-12-03 02:24:20 -0300

    added README.md, better app design

---
+++ README.md
@@ -0,0 +1,20 @@
+**noteapp** is a note taking app made in Python and SQLite3 to help you track your ideas efficiently and quickly.
+
+### Examples
+---
+|         Main Menu          |         Notes Menu          |
+| :--------------------: | :--------------------------: |
+| ![](examples/example2.png) | ![](examples/example1.png) |
+
+### Usage
+---
+- **Show notes**: Shows all the notes a user might have stored, if a user hasn't created notes yet, they can go back to the menu and create a new note.
+- **New note**: Allows a user to create a new note. Contents are stored locally.
+- **Modify note**: Select an existing note and modify its content. *Will reset its state to undone*.
+
+The options for existing notes are the following:
+
+- **Mark as done/undone**: Mark an existing note as visually done or undone depending on its current state.
+- **Modify this note**: Modifies the selected note.
+- **Delete this note**: Deletes the note permanently, requires user confirmation.
+---
--- app.py
+++ app.py
@@ -2,103 +2,143 @@ import sqlite3
 import os
 import sys
 import signal
+import time
 from pick import pick # https://github.com/wong2/pick
 
-dbConnection = sqlite3.connect("notes.db", timeout=5) # Connection to the DB
-dbCursor = dbConnection.cursor() # Cursor to the connection, allows interaction with the DB
-
-# Color palletes for ANSI color coding
-GREEN = "\u001b[32m" # ANSI Color codes, inspired by wordle.c from CS50
-RESET = "\u001b[0m" # More on ANSI codes: https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+# Color palettes for ANSI color coding. Inspired by wordle.c from CS50
+# Read more on ANSI codes here: https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+GREEN = "\u001b[32m"
+RED = "\u001b[31m"
+YELLOW = "\u001b[33m"
+CYAN = "\u001b[36m"
+RESET = "\u001b[0m"
 
 def cls(): # Clear console, if OS is Windows, use 'cls' else use 'clear' - https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
     os.system('cls' if os.name=='nt' else 'clear')
 
 def mainMenu(): # Main menu
-    options = ["Show all notes", "New note", "Modify existing note", "Exit"]
-    option, index = pick(options, indicator="> ")
+    options = ["Show notes", "New note", "Modify note", "Exit"]
+    option, index = pick(options, indicator="○ ", title="Manage your notes")
     cls()
     match index:
         case 0:
-            id = mostrarNotas() # This function should return an ID from the selected note, then provide another option menu to either
+            id = showNotes() # This function should return an ID from the selected note, then provide another option menu to either
             subMenuNote(id)
         case 1:
-            cargarNota()
+            newNote()
             mainMenu()
         case 2:
-            id = mostrarNotas()
-            modificarNota(id)
+            id = showNotes()
+            modifyNote(id)
             mainMenu()
         case 3:
             exit()
 
 def subMenuNote(id):
-    options = ["Modify note", "Delete note", "Back"]
-    option, index = pick(options, indicator="> ")
+    cls()
+    options = ["Mark as done/undone", "Modify this note", "Delete this note", "Back"]
+    option, index = pick(options, indicator="○ ", title='Options')
     match index:
         case 0:
-            modificarNota(id)
+            markNoteDone(id)
             mainMenu()
         case 1:
-            eliminarNota(id)
+            modifyNote(id)
             mainMenu()
         case 2:
+            deleteNote(id)
             mainMenu()
+        case 3:
+            mainMenu()
+
+def markNoteDone(id):
+    s = dbCursor.execute(f"SELECT state FROM notes WHERE id = {id}")
+    s = s.fetchone()[0]
+    if s == 0:
+        text = dbCursor.execute(f"SELECT text FROM notes WHERE id = {id}")
+        text = text.fetchone()[0]
+        dbCursor.execute("UPDATE notes SET text = ?, state = ? WHERE id = ?", (text, 1, id))
+    elif s == 1:
+        text = dbCursor.execute(f"SELECT text FROM notes WHERE id = {id}")
+        text = text.fetchone()[0]
+        dbCursor.execute("UPDATE notes SET text = ?, state = ? WHERE id = ?", (text, 0, id))
 
 def returnQueryText(id): # Returns an individual string from the "text" field matching an ID.
     n = dbCursor.execute(f"SELECT text FROM notes WHERE id = {id}")
     return n.fetchone()[0]
 
-def cargarNota(): # Writes a new entry with an automatically assigned ID
-    text = input("New note - ")
+def newNote(): # Writes a new entry with an automatically assigned ID
+    text = ""
+    print("Press Ctrl-C to exit the program.\n" + GREEN + "Creating new note" + RESET)
+    while text == "":
+        text = input("> ")
+        if text == "":
+            print("Note cannot be empty.")
     dbCursor.execute("INSERT INTO notes (text) VALUES (?)", [text])
 
-def modificarNota(id): # Modifies a row's text field with a given ID
-    print(GREEN + f'Modifying: "{returnQueryText(id)}"' + RESET)
-    newText = input("New content - ")
-    dbCursor.execute("UPDATE notes SET text = ? WHERE id = ?", (newText, id))
+def modifyNote(id): # Modifies a row's text field with a given ID
+    print("Press Ctrl-C to exit the program.\n" + GREEN + f'Modifying: "{returnQueryText(id)}"' + RESET)
+    newText = ""
+    while newText == "":
+        newText = input("> ")
+        if newText == "":
+            print("Note cannot be empty.")
+    dbCursor.execute("UPDATE notes SET text = ?, state = ? WHERE id = ?", (newText, 0, id))
 
-def eliminarNota(id): # Removes a row matching by ID
+def deleteNote(id): # Removes a row matching by ID
     option = ""
     print("Are you sure you want to delete this note? [Y/N]\n", GREEN + f'"{returnQueryText(id)}"' + RESET)
     options = ["Y", "N"]
-    #while option != "N" and option != "Y":
     while option not in options:
         option = input("> ").upper()
-        if option != "N" and option != "Y":
-            print("Please choose [Y/N]")
+        if option not in options:
+            print("Please select a valid option. [Y/N]")
     if option == "N":
         subMenuNote(id)
     elif option == "Y":
         dbCursor.execute(f"DELETE FROM notes WHERE id = {id}")
 
-def mostrarNotas(): # Displays all the notes available
+def showNotes(): # Displays all the notes available
     notas = dbCursor.execute("SELECT * FROM notes")
     options = [] # Create two lists, one with the options and another one with the DB ID so we can match indexes and get the ID data for later
     ids = [] # This is done because the pick() library can only work with lists, not dictionaries.
     for nota in notas:
         ids.append(nota[0])
-        options.append(f"- {nota[1]}")
-    options.append("Go to Main Menu")
-    option, index = pick(options, indicator="> ")
-    if option == "Go to Main Menu":
+        if nota[2] == 0:
+            options.append(f"□ {nota[1]}")
+        else:
+            text = '\u0336'.join(nota[1]) + '\u0336'
+            options.append(f"■ {text}")
+    options.append("Back")
+    option, index = pick(options, indicator="○ ", title="Showing all notes, select a note for more options")
+    if option == "Back": # Adds a final item to the list with the sole purpose of going back to the menu
         mainMenu()
     else:
         return ids[index] # Returns the DB ID of the selected note for later
 
-def handler(signum, frame):
-    print('Signal handler called with signal', signum)
-
 def exit():
+    cls()
     dbConnection.commit()
     dbConnection.close()
+    print("Goodbye! Thank you " + YELLOW + "CS50" + RESET + " for everything!" + RED + " <3" + RESET)
+    time.sleep(1)
     sys.exit()
 
-try:
-    signal.signal(signal.SIGTSTP, signal.SIG_IGN) # Ignores CTRL-Z
-    mainMenu()
-except KeyboardInterrupt:
-    print("Exiting unexpectedly... Please use the main menu next time!")
-    dbConnection.commit()
-
+if __name__ == "__main__":
+    dbConnection = sqlite3.connect("notes.db", timeout=5) # Connection to the DB
+    sqlite_create_table_query = '''CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                    text TEXT NOT NULL, state INTEGER NOT NULL DEFAULT 0);'''
+    dbCursor = dbConnection.cursor() # Cursor to the connection, allows interaction with the DB
+    try:
+        dbCursor.execute(sqlite_create_table_query) # Try to create the tables required, in case they already exist, start executing the program.
+    except sqlite3.OperationalError:
+        ...
+    finally:        
+        try:
+            signal.signal(signal.SIGTSTP, signal.SIG_IGN) # Ignores Ctrl-Z and Ctrl-D
+            mainMenu()
+        except KeyboardInterrupt: #Ctrl-C is handled this exception
+            cls()
+            print(YELLOW + "Exiting unexpectedly... Please use the main menu next time!" + RESET)
+            dbConnection.commit()
 
